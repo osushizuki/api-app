04ab034bb023b6648659f86c62971936c640a94c
osushizuki <imaokanorihiko@icloud.com>
2021-12-14 21:32:01 +0900

    コントローラー

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,21 @@
 class CategoriesController < ApplicationController
+
+  def index
+  end
+
+  def create
+    category = Category.new(category_params)
+    if category.save
+      render json: user, status: :created
+    else
+      render json: user.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def category_params
+    params.permit(:name)
+  end
+
 end
--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -1,2 +1,23 @@
 class IdeasController < ApplicationController
+  before_action :set_idea, only: [:show]
+
+  def index
+    ideas = Idea.all
+    render json: { data: ideas }
+  end
+
+  def create
+    idea = Idea.new(idea_params)
+  end
+
+  private
+
+  def set_idea
+    @idea = Idea.find(params[:category_id])
+  end
+
+  def idea_params
+    params.permit(:body,:category_id)
+  end
+
 end
